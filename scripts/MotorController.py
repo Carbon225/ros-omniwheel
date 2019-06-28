2d62dbefee3b1f17bd41d53da647fbc4ea7bbd72
Carbon225 <carbon.szlachta@gmail.com>
2019-06-28 08:09:04 -0700

    Motor enable pin

--- scripts/MotorController.py
+++ scripts/MotorController.py
@@ -4,24 +4,32 @@ from std_msgs.msg import Int16
 import RPi.GPIO as GPIO
 
 class MotorDC:
-    def __init__(self, pin_a, pin_b):
+    def __init__(self, pin_a, pin_b, pin_en):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin_a, GPIO.OUT)
         GPIO.setup(pin_b, GPIO.OUT)
+        GPIO.setup(pin_en, GPIO.OUT)
 
         self.pinA = GPIO.PWM(pin_a, 100)
         self.pinB = GPIO.PWM(pin_b, 100)
+        self.pinEn = pin_en
 
         self.pinA.start(0)
         self.pinB.start(0)
 
     def drive(self, speed):
-        if speed >= 0:
+        if speed > 0:
+            GPIO.output(self.pinEn, 1)
             self.pinA.ChangeDutyCycle(speed)
             self.pinB.ChangeDutyCycle(0)
-        else:
+        elif speed < 0:
+            GPIO.output(self.pinEn, 1)
             self.pinA.ChangeDutyCycle(0)
-            self.pinB.ChangeDutyCycle(-speed)
+            self.pinB.ChangeDutyCycle(abs(speed))
+        else:
+            GPIO.output(self.pinEn, 0)
+            self.pinA.ChangeDutyCycle(0)
+            self.pinB.ChangeDutyCycle(0)
 
 motor = 0
 
@@ -35,9 +43,10 @@ def listener():
 
     pin_a = rospy.get_param('~pin_a')
     pin_b = rospy.get_param('~pin_b')
+    pin_en = rospy.get_param('~pin_en')
 
     global motor
-    motor = MotorDC(pin_a, pin_b)
+    motor = MotorDC(pin_a, pin_b, pin_en)
 
     rospy.spin()
 
