d16e5a40dfeddf0b70528af1a81a7930f5287f6a
Carbon225 <carbon225@protonmail.com>
2023-11-04 16:30:45 +0100

    float

--- src/omniwheel_node.cpp
+++ src/omniwheel_node.cpp
@@ -4,16 +4,16 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/WrenchStamped.h>
-#include <std_msgs/Int16.h>
+#include <std_msgs/Float64.h>
 
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_listener.h>
 
 
-ros::Publisher motorSpeedPub;
-ros::Publisher motorWrenchPub;
-tf2_ros::Buffer tfBuffer;
+static ros::Publisher motorSpeedPub;
+static ros::Publisher motorWrenchPub;
+static tf2_ros::Buffer tfBuffer;
 
 void controlCallback(const geometry_msgs::Twist &twist);
 
@@ -25,10 +25,10 @@ int main(int argc, char **argv)
 
     ros::Subscriber controlSub = nh.subscribe("cmd_vel", 512, controlCallback);
 
-    /*ros::Publisher*/ motorSpeedPub = nh.advertise<std_msgs::Int16>("motor_speed", 512);
+    /*ros::Publisher*/ motorSpeedPub = nh.advertise<std_msgs::Float64>("motor", 1);
     // motorSpeedPub_ptr = &motorSpeedPub;
 
-    /*ros::Publisher*/ motorWrenchPub = nh.advertise<geometry_msgs::WrenchStamped>("motor_wrench", 1);
+    // /*ros::Publisher*/ motorWrenchPub = nh.advertise<geometry_msgs::WrenchStamped>("motor_wrench", 1);
     // motorWrenchPub_ptr = &motorWrenchPub;
 
     tf2_ros::TransformListener tfListener(tfBuffer);
@@ -52,7 +52,7 @@ void controlCallback(const geometry_msgs::Twist &twist)
     ros::param::get("~wheel_id", wheel_id);
     try {
         // get wheel position from transform
-        geometry_msgs::TransformStamped wheelTransform = tfBuffer.lookupTransform("base_link", "wheel"+std::to_string(wheel_id), ros::Time(0));
+        geometry_msgs::TransformStamped wheelTransform = tfBuffer.lookupTransform("base_link", "wheel"+std::to_string(wheel_id)+"_link", ros::Time(0));
 
         wheel_x = wheelTransform.transform.translation.x;
         wheel_y = wheelTransform.transform.translation.y;
@@ -88,16 +88,16 @@ void controlCallback(const geometry_msgs::Twist &twist)
     // align wheel plane with Y axis and project speed vector to new wheel plane
     const double wheel_speed = x * sin(-wheel_angle) + y * cos(-wheel_angle);
 
-    geometry_msgs::WrenchStamped wrench;
-    wrench.header.frame_id = "wheel"+std::to_string(wheel_id);
-    wrench.header.stamp = ros::Time::now();
+    // geometry_msgs::WrenchStamped wrench;
+    // wrench.header.frame_id = "wheel"+std::to_string(wheel_id);
+    // wrench.header.stamp = ros::Time::now();
 
-    wrench.wrench.force.y = wheel_speed;
-    wrench.wrench.torque.x = wheel_speed;
+    // wrench.wrench.force.y = wheel_speed;
+    // wrench.wrench.torque.x = wheel_speed;
 
-    motorWrenchPub.publish(wrench);
+    // motorWrenchPub.publish(wrench);
 
-    std_msgs::Int16 motor_control;
-    motor_control.data = -wheel_speed * 100;
+    std_msgs::Float64 motor_control;
+    motor_control.data = -wheel_speed;
     motorSpeedPub.publish(motor_control);
 }
